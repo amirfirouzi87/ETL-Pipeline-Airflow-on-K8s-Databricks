bafea6dc8a0392e7630f15902b79dccd6b61546d
amirfirouzi87 <amirfiroozi87@gmail.com>
2025-11-17 14:48:09 +0000

    added new dag

---
+++ dags/testdag.py
@@ -0,0 +1,15 @@
+from airflow import DAG, task
+from datetime import datetime
+
+with DAG(
+    dag_id="testdag",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval="@daily",
+    catchup=False,
+) as dag:
+
+    @task
+    def hello_task():
+        print("Hello, Airflow!")
+
+    hello_task()
