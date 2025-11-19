ec22bbfea027eaf28bb122893913dabc97a4d233
amirfirouzi87 <amirfiroozi87@gmail.com>
2025-11-19 08:35:28 +0000

    youtube dag modified

--- dags/api/yt_vids_data.py
+++ dags/api/yt_vids_data.py
@@ -93,7 +93,7 @@ def extract_video_data(video_ids):
 
 @task
 def save_to_json(extracted_data):
-    file_path = f"./data/YT_data_{date.today()}.json"
+    file_path = f"./tmp/YT_data_{date.today()}.json"
 
     with open(file_path, 'w', encoding='utf-8') as json_outfile:
         json.dump(extracted_data, json_outfile, ensure_ascii=False, indent=4)
