9275319ed4af7ea9ab6274060ad22fc1b377753a
amirfirouzi87 <amirfiroozi87@gmail.com>
2025-11-20 14:23:26 +0000

    added new dag

--- dags/yt_etl_dag.py
+++ dags/yt_etl_dag.py
@@ -1,4 +1,5 @@
 from airflow.sdk import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
 from api.yt_vids_data import get_playlist_id, get_video_metadata, extract_video_data, save_to_json_upload_s3
 from datetime import timedelta
 
@@ -19,4 +20,11 @@ with DAG(
     video_metadata = get_video_metadata(playlist_id)
     extracted_data = extract_video_data(video_metadata)
     save_in_json = save_to_json_upload_s3(extracted_data)
-    playlist_id >> video_metadata >> extracted_data >> save_in_json
+
+    run_databricks_job = DatabricksRunNowOperator(
+        task_id='run_transformation_job',
+        databricks_conn_id='databricks_default',
+        job_id=253944526787423  # Replace with your actual Databricks job ID
+    )
+
+    playlist_id >> video_metadata >> extracted_data >> save_in_json >> run_databricks_job
