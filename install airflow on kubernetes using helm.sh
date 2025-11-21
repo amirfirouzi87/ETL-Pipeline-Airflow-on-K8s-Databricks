6402fb21caea7c1a1d2f1c87c218909735a3ce2b
amirfirouzi87 <amirfiroozi87@gmail.com>
2025-11-21 13:38:21 +0000

    made small changes

--- install airflow on kubernetes using helm.sh
+++ install airflow on kubernetes using helm.sh
@@ -1,12 +1,12 @@
 kind delete cluster --name kind
-kind create cluster --image kindest/node:v1.27.0 --config k8s/clusters/kind-cluster.yaml
+kind create cluster --image kindest/node:v1.27.0 --config kind/clusters/kind-cluster.yaml
 
 helm repo add apache-airflow https://airflow.apache.org
 helm repo update
 helm show values apache-airflow/airflow > airflow-values.yaml
 
 
-docker build --pull --tag my-airflow-image:0.1 -f CICD/Dockerfile .
+docker build --pull --tag my-airflow-image:0.1 -f docker/Dockerfile .
 kind load docker-image my-airflow-image:0.1
 
 kubectl create namespace airflow
@@ -16,9 +16,8 @@ kubectl apply -f k8s/volumes/airflow-logs-pv.yaml
 kubectl apply -f k8s/volumes/airflow-logs-pvc.yaml
 
 
-helm install airflow apache-airflow/airflow \
+helm upgrade --install airflow apache-airflow/airflow \
     --namespace airflow -f chart/airflow-override-values.yaml \
-    --set-string images.airflow.tag="0.1" \
     --debug
 
 
